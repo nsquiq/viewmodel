7350574e815ddcc17a98c779148d51ff5cfc6791
Nuray <210103331@stu.sdu.edu.kz>
2023-11-10 23:58:28 +0600

    2

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -1,10 +1,43 @@
 package com.example.unscramble.ui
 
 import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.allWords
 import com.example.unscramble.ui.model.GameUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 
-// Game UI state
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+// Game UI stat
+
+// Backing property to avoid state updates from other classes
 private val _uiState = MutableStateFlow(GameUiState())
+val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+private lateinit var currentWord: String
+
+private fun pickRandomWordAndShuffle(): String {
+    // Continue picking up a new random word until you get one that hasn't been used before
+    currentWord = allWords.random()
+
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+
+    if (usedWords.contains(currentWord)) {
+        return pickRandomWordAndShuffle()
+    } else {
+        usedWords.add(currentWord)
+        return shuffleCurrentWord(currentWord)
+    }
+}
+private fun shuffleCurrentWord(word: String): String {
+    val tempWord = word.toCharArray()
+    // Scramble the word
+    tempWord.shuffle()
+    while (String(tempWord).equals(word)) {
+        tempWord.shuffle()
+    }
+    return String(tempWord)
+}
 class GameViewModel : ViewModel() {
 }
