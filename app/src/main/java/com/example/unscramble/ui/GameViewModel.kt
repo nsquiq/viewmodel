da343087f5c774b04d2e509e19a0a76d16e7e3a6
Nuray <210103331@stu.sdu.edu.kz>
2023-11-11 00:04:20 +0600

    correct

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -10,34 +10,44 @@ import kotlinx.coroutines.flow.asStateFlow
 
 // Game UI stat
 
-// Backing property to avoid state updates from other classes
-private val _uiState = MutableStateFlow(GameUiState())
-val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+class GameViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
 
-private lateinit var currentWord: String
-
-private fun pickRandomWordAndShuffle(): String {
-    // Continue picking up a new random word until you get one that hasn't been used before
-    currentWord = allWords.random()
-
+    private lateinit var currentWord: String
     private var usedWords: MutableSet<String> = mutableSetOf()
 
+    init {
+        resetGame()
+    }
 
-    if (usedWords.contains(currentWord)) {
-        return pickRandomWordAndShuffle()
-    } else {
-        usedWords.add(currentWord)
-        return shuffleCurrentWord(currentWord)
+    private fun pickRandomWordAndShuffle(): String {
+        // Continue picking up a new random word until you get one that hasn't been used before
+        currentWord = allWords.random()
+
+
+
+
+        if (usedWords.contains(currentWord)) {
+            return pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            return shuffleCurrentWord(currentWord)
+        }
     }
-}
-private fun shuffleCurrentWord(word: String): String {
-    val tempWord = word.toCharArray()
-    // Scramble the word
-    tempWord.shuffle()
-    while (String(tempWord).equals(word)) {
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        // Scramble the word
         tempWord.shuffle()
+        while (String(tempWord).equals(word)) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
     }
-    return String(tempWord)
-}
-class GameViewModel : ViewModel() {
+
 }
