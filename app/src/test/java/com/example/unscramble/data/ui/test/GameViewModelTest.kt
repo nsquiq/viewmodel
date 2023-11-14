65c0c0909b025dd31b0c96da51cf3652c655188e
Nuray <210103331@stu.sdu.edu.kz>
2023-11-14 11:36:38 +0600

    2

--- app/src/test/java/com/example/unscramble/data/ui/test/GameViewModelTest.kt
+++ app/src/test/java/com/example/unscramble/data/ui/test/GameViewModelTest.kt
@@ -5,6 +5,7 @@ import com.example.unscramble.data.getUnscrambledWord
 import com.example.unscramble.ui.GameViewModel
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
 class GameViewModelTest {
@@ -25,5 +26,17 @@ class GameViewModelTest {
     }
     companion object {
         private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
+    }
+    @Test
+    fun gameViewModel_IncorrectGuess_ErrorFlagSet() {
+
+        val incorrectPlayerWord = "and"
+        viewModel.updateUserGuess(incorrectPlayerWord)
+        viewModel.checkUserGuess()
+
+        val currentGameUiState = viewModel.uiState.value
+
+        assertEquals(0, currentGameUiState.score)
+        assertTrue(currentGameUiState.isGuessedWordWrong)
     }
 }
